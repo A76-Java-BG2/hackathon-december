67cd9b4d9143b33bf64eabfed84a380f17102050
Marian Todorov <marian.sf.2906@gmail.com>
2025-12-13 21:08:10 +0200

    added new functionality and description to methods: contains, endWith, firstIndexOf

--- src/com/company/core/strings/StringHelpers.java
+++ src/com/company/core/strings/StringHelpers.java
@@ -15,16 +15,63 @@ public class StringHelpers {
         return null;
     }
 
+    /**
+     * Checks if source contains a symbol.
+     *
+     * @param source The string to check
+     * @param symbol The character to check for
+     * @return  true if symbol is within source or false if not
+     *
+     * @author Mariyan Todorov
+     */
     public static boolean contains(String source, char symbol) {
-        return false;
-    }
+        boolean isContains = false;
 
+        for (int i = 0; i < source.length(); i++) {
+            char temp = source.charAt(i);
+            if(temp == symbol){
+                isContains = true;
+                break;
+            }
+        }
+        return isContains;
+    }
+    /**
+     * Checks if the string source ends with the given character.
+     *
+     * @param source The string to check
+     * @param target The character to check for
+     * @return  true if the string ends with target, else false
+     *
+     * @author Mariyan Todorov
+     */
     public static boolean endsWith(String source, char target) {
-        return false;
+        boolean isEndsWith = false;
+        if(source.length() != 0){
+            char lastLetter = source.charAt(source.length() - 1);
+            if(lastLetter == target){
+                isEndsWith = true;
+            }
+        }
+        return isEndsWith;
     }
-
+    /**
+     * Finds the first index of target within source
+     *
+     * @param source The string to check
+     * @param target The character to check for
+     * @return The first index of target within source, otherwise, -1
+     *
+     * @author Mariyan Todorov
+     */
     public static int firstIndexOf(String source, char target) {
-        return 0;
+
+        for (int i = 0; i < source.length(); i++) {
+            if(target == source.charAt(i)){
+                return i;
+            }
+        }
+        return -1;
     }
 
     public static int lastIndexOf(String source, char symbol) {

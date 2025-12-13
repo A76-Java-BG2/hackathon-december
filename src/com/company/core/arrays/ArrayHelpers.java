aa41d1cd548ca0f65e7b8685fbe587582614e99b
Marian Todorov <marian.sf.2906@gmail.com>
2025-12-13 21:03:45 +0200

    added new functionality and description to methods: contains, copy, copyFrom

--- src/com/company/core/arrays/ArrayHelpers.java
+++ src/com/company/core/arrays/ArrayHelpers.java
@@ -15,17 +15,66 @@ public class ArrayHelpers {
         return new int[1];
     }
 
+    /**
+     * Check if <code> element</code> is contained in the string.
+     *
+     * @param source The string in which we are searching for the element.
+     * @param element The string we are looking for
+     * @return boolean value which show us if the string is in the text.
+     *
+     * @author Mariyan Todorov
+     */
     public static boolean contains(int[] source, int element) {
-        return false;
+        boolean isContain = false;
+        for (int i = 0; i < source.length; i++) {
+            if (source[i] == element){
+                isContain = true;
+                break;
+            }
+        }
+        return isContain;
     }
 
+    /**
+     * Create new array from other one.
+     *
+     * @param sourceArray The array to copy from
+     * @param destinationArray The array to copy to
+     * @param count The number of elements to copy
+     * 
+     *
+     * @author Mariyan Todorov
+     */
     public static void copy(int[] sourceArray, int[] destinationArray, int count) {
-
+        for (int i = 0; i < count; i++) {
+            if(i >= sourceArray.length){
+                destinationArray[i] = 0;
+            }else{
+                destinationArray[i] = sourceArray[i];
+            }
+        }
     }
-
+    /**
+     * Copies elements from sourceArray, starting from sourceStartIndex into destinationArray,
+     * starting from destStartIndex, taking count elements.
+     *
+     * @param sourceArray The array to copy from
+     * @param sourceStartIndex The starting index in sourceArray
+     * @param destinationArray The array to copy to
+     * @param destStartIndex The starting index in destinationArray
+     * @param count The number of elements to copy
+     *
+     *
+     * @author Mariyan Todorov
+     */
     public static void copyFrom(int[] sourceArray, int sourceStartIndex,
                                 int[] destinationArray, int destStartIndex, int count) {
 
+            int destIndex = destStartIndex;
+        for (int i = sourceStartIndex; i < count; i++) {
+            destinationArray[destIndex] = sourceArray[i];
+            destIndex++;
+        }
     }
 
     public static void fill(int[] source, int element) {

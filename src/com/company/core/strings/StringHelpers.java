1df6cc0dd6f4680365dcbb7a463ba0dc1654333c
stefanova-kalina <stefanova_kalina@yahoo.com>
2025-12-13 21:48:01 +0000

    commit Abbreviate, Capitalize, Concat in StringHelpers

--- src/com/company/core/strings/StringHelpers.java
+++ src/com/company/core/strings/StringHelpers.java
@@ -3,16 +3,62 @@ package com.company.core.strings;
 @SuppressWarnings("StringConcatenationInLoop")
 public class StringHelpers {
 
+    /**
+     * Abbreviates <code>source</code> to <code>maxLength</code> number of characters and adds ellipses.
+     *
+     * @param source The original string to modify
+     * @param maxLength The maximum length of the resulting string
+     * @return A string that represents the abbreviation of source with ellipses.
+     *
+     * @author Kalina Stefanova
+     */
+
     public static String abbreviate(String source, int maxLength) {
-        return null;
+        String result;
+        if (maxLength > source.length()) {
+            result = source;
+        } else if (source.isEmpty()) {
+            result = "";
+        } else{
+            result = source.substring(0,maxLength) + "...";
+        }
+        return result;
     }
+
+    /**
+     * Capitalizes the first character of <code>source</code>. If <code>source</code> is empty, returns empty string.
+     *
+     * @param source The original string to modify
+     * @return The capitalized string or an empty string if <code>source</code> is an emptry string.
+     *
+     * @author Kalina Stefanova
+     */
 
     public static String capitalize(String source) {
-        return null;
+
+        if (!source.isEmpty()) {
+            char firstChar = source.charAt(0);
+            firstChar -= 32;
+            source = firstChar + source.substring(1);
+        }
+
+        return source;
     }
 
+    /**
+     * Concatenates <code>string1</code> to <code>string2</code>.
+     *
+     * @param string1 The left part of the string
+     * @param string2 The right part of the string
+     * @return A string that represents the concatenation of <code>string1</code> followed by <code>string2<code>'s characters.
+     *
+     * @author Kalina Stefanova
+     */
+
+
     public static String concat(String string1, String string2) {
-        return null;
+
+        return string1 + string2;
     }
 
     public static boolean contains(String source, char symbol) {
